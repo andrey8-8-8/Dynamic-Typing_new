0d35bf8abf377d3a042dded421f17bee55ae340c
andre <andreymaster100@gmal.com>
2024-12-20 11:40:41 +0300

    ервый коммит

---
+++ start1.py
@@ -0,0 +1,24 @@
+name='andrey'
+print(name,type(name))
+age=57
+print(age)
+print(age,type(age))
+new_age=age+15
+print(new_age)
+is_student=True
+print(is_student)
+print(is_student,type(is_student))
+
+name = 'andrey'                 # Объявляем переменную name и присваиваем ей строковое значение
+print(name, type(name))         # Печатаем имя и его тип (строка)
+
+age = 57                        # Объявляем переменную age и присваиваем ей целочисленное значение
+print(age)                      # Печатаем значение age
+print(age, type(age))          # Печатаем значение age и его тип (целое число)
+
+new_age = age + 15             # Создаем новую переменную new_age, увеличивая age на 15
+print(new_age)                 # Печатаем значение new_age
+
+is_student = True               # Объявляем переменную is_student и присваиваем ей булевое значение
+print(is_student)               # Печатаем значение is_student
+print(is_student, type(is_student))  # Печатаем значение is_student и его тип (булевый)
